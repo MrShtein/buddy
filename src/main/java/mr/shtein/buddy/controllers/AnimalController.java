3b50ec9fbf15e6b0ca1bd56009e2eda1f46daf22
Mr.Shtein <mr.shtein61@yandex.ru>
2021-09-05 15:42:06 +0300

    [feat] Добавил возможность заносить новых животных

--- src/main/java/mr/shtein/buddy/controllers/AnimalController.java
+++ src/main/java/mr/shtein/buddy/controllers/AnimalController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,7 +28,13 @@ public class AnimalController {
 
     @GetMapping ("/api/v1/getAnimals")
     public ResponseEntity<List<Animal>> getAnimalsByFilter() {
-        return new ResponseEntity<>(animalService.getAllAnimals(), HttpStatus.OK);
+        return new ResponseEntity<>(animalService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/v1/addAnimal")
+    public ResponseEntity<Boolean> addAnimal(@RequestBody Animal animal) {
+        Boolean  response = animalService.add(animal);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
--- src/main/java/mr/shtein/buddy/services/AnimalService.java
+++ src/main/java/mr/shtein/buddy/services/AnimalService.java
@@ -18,10 +18,22 @@ public class AnimalService {
         this.animalRepository = animalRepository;
     }
 
-    public List<Animal> getAllAnimals() {
+    public List<Animal> getAll() {
         Iterable<Animal> animalsIter = animalRepository.findAll();
         ArrayList<Animal> animals = new ArrayList<>();
         animalsIter.forEach(animals::add);
         return animals;
     }
+
+    public Boolean add(Animal animal) {
+        try {
+            animalRepository.save(animal);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+
 }
