59c078ca0b32ea33a65ca6cc038661671a9987e5
Mr.Shtein <mr.shtein61@yandex.ru>
2021-09-10 18:19:12 +0300

    Revert "[refactor] Изменил endpoint"
    This reverts commit 79b38f74b8ceab12a474f0440f7af155e1eb685f.

--- src/main/java/mr/shtein/buddy/controllers/AnimalController.java
+++ src/main/java/mr/shtein/buddy/controllers/AnimalController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 import mr.shtein.buddy.models.Animal;
 import mr.shtein.buddy.services.AnimalService;
+import mr.shtein.buddy.viewmodel.AnimalDTO;
 
 @RestController
 @Validated
@@ -26,14 +31,21 @@ public class AnimalController {
         this.animalService = animalService;
     }
 
-    @GetMapping ("/api/v1/getAnimals")
-    public ResponseEntity<List<Animal>> getAnimalsByFilter() {
-        return new ResponseEntity<>(animalService.getAll(), HttpStatus.OK);
+    @GetMapping("/api/v1/getAnimals")
+    public ResponseEntity<List<AnimalDTO>> getAnimalsByFilter() {
+        List<Animal> animals = animalService.getAll();
+        List<AnimalDTO> animalsDTO = new ArrayList<>();
+        for (Animal animal : animals) {
+            AnimalDTO currentAnimalDTO = new AnimalDTO();
+            currentAnimalDTO.from(animal);
+            animalsDTO.add(currentAnimalDTO);
+        }
+        return new ResponseEntity<>(animalsDTO, HttpStatus.OK);
     }
 
     @PostMapping("/api/v1/addAnimal")
     public ResponseEntity<Boolean> addAnimal(@RequestBody Animal animal) {
-        Boolean  response = animalService.add(animal);
+        Boolean response = animalService.add(animal);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
