79b38f74b8ceab12a474f0440f7af155e1eb685f
Mr.Shtein <mr.shtein61@yandex.ru>
2021-09-10 16:26:25 +0300

    [refactor] Изменил endpoint

--- src/main/java/mr/shtein/buddy/controllers/AnimalController.java
+++ src/main/java/mr/shtein/buddy/controllers/AnimalController.java
@@ -9,16 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 
 import mr.shtein.buddy.models.Animal;
 import mr.shtein.buddy.services.AnimalService;
-import mr.shtein.buddy.viewmodel.AnimalDTO;
 
 @RestController
 @Validated
@@ -31,21 +26,14 @@ public class AnimalController {
         this.animalService = animalService;
     }
 
-    @GetMapping("/api/v1/getAnimals")
-    public ResponseEntity<List<AnimalDTO>> getAnimalsByFilter() {
-        List<Animal> animals = animalService.getAll();
-        List<AnimalDTO> animalsDTO = new ArrayList<>();
-        for (Animal animal : animals) {
-            AnimalDTO currentAnimalDTO = new AnimalDTO();
-            currentAnimalDTO.from(animal);
-            animalsDTO.add(currentAnimalDTO);
-        }
-        return new ResponseEntity<>(animalsDTO, HttpStatus.OK);
+    @GetMapping ("/api/v1/getAnimals")
+    public ResponseEntity<List<Animal>> getAnimalsByFilter() {
+        return new ResponseEntity<>(animalService.getAll(), HttpStatus.OK);
     }
 
     @PostMapping("/api/v1/addAnimal")
     public ResponseEntity<Boolean> addAnimal(@RequestBody Animal animal) {
-        Boolean response = animalService.add(animal);
+        Boolean  response = animalService.add(animal);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
