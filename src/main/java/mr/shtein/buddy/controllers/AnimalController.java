9b790a0ddbdb5f6f92e3362cfde28dc4ec2ec30e
Mr.Shtein <mr.shtein61@yandex.ru>
2021-09-11 08:43:21 +0300

    [feat] Пробуем достать фото

--- src/main/java/mr/shtein/buddy/controllers/AnimalController.java
+++ src/main/java/mr/shtein/buddy/controllers/AnimalController.java
@@ -1,18 +1,22 @@
 package mr.shtein.buddy.controllers;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -41,6 +45,14 @@ public class AnimalController {
             animalsDTO.add(currentAnimalDTO);
         }
         return new ResponseEntity<>(animalsDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/v1/photo/{id}")
+    public String getPhoto(@PathVariable String id) throws IOException {
+        String address = System.getProperty("user.home") + "/buddyPhotos/" + id + "/";
+        List<Path> files = Files.list(Paths.get(address)).collect(Collectors.toList());
+        files.stream().forEach(x -> System.out.println(x.getFileName()));
+        return "Ok";
     }
 
     @PostMapping("/api/v1/addAnimal")
